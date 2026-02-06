def02d4e2016e0e64373ee42f38e5b5dfdf1f915
Achraf Tazrouti <s161474@ap.be>
2026-02-06 10:53:22 +0100

    blog detail pagina aangemaakt

--- src/app/pages/blog-detail/blog-detail.component.ts
+++ src/app/pages/blog-detail/blog-detail.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogPost } from '../../models/blog-post.model';
 
 @Component({
-  selector: 'app-blog-detail',
-  imports: [],
-  templateUrl: './blog-detail.html',
-  styleUrl: './blog-detail.css',
+  standalone: true,
+  templateUrl: './blog-detail.component.html',
+  styleUrl: './blog-detail.component.css'
 })
-export class BlogDetail {
+export class BlogDetailComponent {
+  post = signal<BlogPost | null>(null);
 
+  private allPosts: BlogPost[] = [
+    {
+      id: 1,
+      title: 'Eerste stagedag',
+      content: 'Vandaag ben ik gestart aan mijn stage...'
+    },
+    {
+      id: 2,
+      title: 'Wat ik geleerd heb over Angular',
+      content: 'Standalone components en signals zijn 🔥'
+    }
+  ];
+
+  constructor(route: ActivatedRoute) {
+    const id = Number(route.snapshot.paramMap.get('id'));
+    const found = this.allPosts.find(p => p.id === id);
+    this.post.set(found ?? null);
+  }
 }

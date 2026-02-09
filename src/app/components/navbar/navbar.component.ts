9c8838ec7a91f6435feb64c1a244d24ef3d6e8f8
Achraf Tazrouti <s161474@ap.be>
2026-02-09 16:27:39 +0100

    ap logo added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
-
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MenubarModule],
+  imports: [MenubarModule,AvatarModule,BadgeModule,InputTextModule,RippleModule],
   templateUrl: './navbar.component.html',
+  styleUrls:['./navbar.component.css']
 })
 export class NavbarComponent {
   items: MenuItem[] | undefined;

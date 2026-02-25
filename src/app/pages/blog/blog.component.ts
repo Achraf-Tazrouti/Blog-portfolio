2146a43671925902c2f2559ba0a9c188d9bbacab
Achraf Tazrouti <s161474@ap.be>
2026-02-25 15:16:21 +0100

    Add tag filtering to blog page

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BlogCardComponent } from '../../components/blog-card/blog-card.component';
 import { CommonModule } from '@angular/common';
 import { BlogService } from '../../services/blog.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { startWith } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
@@ -9,11 +13,41 @@ import { BlogService } from '../../services/blog.service';
   styleUrls: ['./blog.component.scss'],
   imports: [
     CommonModule,
-    BlogCardComponent
+    BlogCardComponent,
+    ReactiveFormsModule,
+    MultiSelectModule
   ]
 })
 export class BlogComponent {
   posts;
+  selectedTagsControl = new FormControl<string[]>([], { nonNullable: true });
+  selectedTags = toSignal(
+    this.selectedTagsControl.valueChanges.pipe(
+      startWith(this.selectedTagsControl.value)
+    ),
+    { initialValue: [] }
+  );
+  tags = computed(() => {
+    const all = new Set<string>();
+    for (const post of this.posts()) {
+      (post.skills ?? []).forEach((skill) => all.add(skill));
+      (post.tags ?? []).forEach((tag) => all.add(tag));
+    }
+    return Array.from(all).sort((a, b) => a.localeCompare(b));
+  });
+  tagOptions = computed(() =>
+    this.tags().map((tag) => ({ label: tag, value: tag }))
+  );
+  filteredPosts = computed(() => {
+    const selected = this.selectedTags();
+    const allPosts = this.posts();
+    if (!selected.length) return allPosts;
+    return allPosts.filter((post) => {
+      const skills = post.skills ?? [];
+      const tags = post.tags ?? [];
+      return selected.some((tag) => skills.includes(tag) || tags.includes(tag));
+    });
+  });
 
   constructor(private blogService: BlogService) {
     this.posts = this.blogService.posts;
